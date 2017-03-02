704cefc9738967fb91de8cd24758c6ae50bb7a0c
TeigeSB <teigebradford@students.berkeley.net>
2017-03-02 11:24:55 -0800

    commit - can now search for coordinates of city/country

--- java.js
+++ java.js
@@ -2,10 +2,10 @@
  * Created by h205p3 on 2/15/17.
  */
 
-var lonLat = ""
-
 function search() {
     var userSearch = document.getElementById("userSearch").value;
+    var lon = 0;
+    var lat = 0;
     $.ajax({
         url: ["https://api.mapbox.com/geocoding/v5/mapbox.places/" + userSearch + ".json?access_token=pk.eyJ1IjoidGVpZ2VzYiIsImEiOiJjaXpyYmpzd3gwMDF1MnFwY29qbG9qYzVsIn0.sxgtFCkke6Aqlie1kMdxTw"],
         type: 'GET',
@@ -15,12 +15,31 @@ function search() {
             console.log(result);
             console.log(result.features[0].geometry.coordinates[0]);
             console.log(result.features[0].geometry.coordinates[1]);
-            lonLat = result
+            lon = result.features[0].geometry.coordinates[0];
+            lat = result.features[0].geometry.coordinates[1];
+            console.log(lon);
+            console.log(lat)
         },
     error: function() {
             alert('Please enter a place! Idiot...');
         }
 });
+    $.ajax({
+        url: ["https://api.darksky.net/forecast/984df1c3ecb015136e10fe33ed3c8205/" + lon + "," + lat],
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(cancer) {
+            console.log(cancer);
+            console.log(lon);
+            console.log(lat)
+        },
+        error: function() {
+            alert('Coordniates not working');
+            console.log(lon);
+            console.log(lat)
+        }
+    });
 }
 
 function someThing() {
