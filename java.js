f47d5e0bd8645a19951131399ff40c6f9ef74c63
TeigeSB <teigebradford@students.berkeley.net>
2017-03-02 10:58:45 -0800

    commit - can now search for coordinates of city/country

--- java.js
+++ java.js
@@ -2,6 +2,7 @@
  * Created by h205p3 on 2/15/17.
  */
 
+var lonLat = ""
 
 function search() {
     var userSearch = document.getElementById("userSearch").value;
@@ -11,11 +12,18 @@ function search() {
     crossDomain: true,
         dataType: 'json',
         success: function(result) {
-            console.log(result)
+            console.log(result);
+            console.log(result.features[0].geometry.coordinates[0]);
+            console.log(result.features[0].geometry.coordinates[1]);
+            lonLat = result
         },
     error: function() {
-            alert('Failed!');
+            alert('Please enter a place! Idiot...');
         }
 });
+}
+
+function someThing() {
+    console.log(lonLat)
 }
 
