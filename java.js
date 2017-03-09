9e6cd980dc254a9f04668e907b8d7322de06d9a8
TeigeSB <teigebradford@students.berkeley.net>
2017-03-09 11:14:16 -0800

    commit - weather works, specifying clothing

--- java.js
+++ java.js
@@ -2,6 +2,8 @@
  * Created by h205p3 on 2/15/17.
  */
 
+var temp = 0
+
 function search() {
     var userSearch = document.getElementById("userSearch").value;
     var lon = 0;
@@ -9,40 +11,65 @@ function search() {
     $.ajax({
         url: ["https://api.mapbox.com/geocoding/v5/mapbox.places/" + userSearch + ".json?access_token=pk.eyJ1IjoidGVpZ2VzYiIsImEiOiJjaXpyYmpzd3gwMDF1MnFwY29qbG9qYzVsIn0.sxgtFCkke6Aqlie1kMdxTw"],
         type: 'GET',
-    crossDomain: true,
+        crossDomain: true,
         dataType: 'json',
-        success: function(result) {
+        success: function (result) {
             console.log(result);
-            console.log(result.features[0].geometry.coordinates[0]);
-            console.log(result.features[0].geometry.coordinates[1]);
-            lon = result.features[0].geometry.coordinates[0];
-            lat = result.features[0].geometry.coordinates[1];
-            console.log(lon);
-            console.log(lat)
+            lat = result.features[0].geometry.coordinates[0];
+            lon = result.features[0].geometry.coordinates[1];
+            weatherCheck(lon, lat);
+            document.getElementById("place").innerHTML = result.features[0].place_name;
+            document.getElementById("userSearch").innerHTML = ""
         },
-    error: function() {
-            alert('Please enter a place! Idiot...');
+        error: function () {
+            alert('Enter a place!');
         }
-});
+    });
+}
+
+function weatherCheck(lon, lat) {
     $.ajax({
-        url: ["https://api.darksky.net/forecast/984df1c3ecb015136e10fe33ed3c8205/" + lon + "," + lat],
+        url: "https://api.darksky.net/forecast/984df1c3ecb015136e10fe33ed3c8205/" + lon + "," + lat,
         type: 'GET',
         crossDomain: true,
         dataType: 'jsonp',
-        success: function(cancer) {
-            console.log(cancer);
-            console.log(lon);
-            console.log(lat)
+        success: function(ok) {
+            console.log(ok);
+            temp = ok.currently.temperature;
+            weatherDress();
+            document.getElementById("temp").innerHTML = "Temperature:" + " " + ok.currently.temperature + " " +"°F";
+            document.getElementById("lives").innerHTML =  "Humidity:" + " " + ok.currently.humidity * 100 + "%";
+
         },
         error: function() {
-            alert('Coordniates not working');
+            alert('Weather not working.');
             console.log(lon);
             console.log(lat)
         }
     });
 }
 
-function someThing() {
-    console.log(lonLat)
+function weatherDress() {
+    var dress = prompt("Do you dress like a male or female?");
+    if (dress = "male".toLowerCase()) {
+        console.log("MAAAN");
+        var how = howToDressM();
+    } else if (dress = "female".toLowerCase()) {
+        console.log("GIIIIRL");
+        var wow = howToDressF();
+    }
+
+}
+
+function howToDressM() {
+    var ans = prompt("Do you want to dress formal or casual?");
+    if (ans = "Formal".toLowerCase()) {
+        if (temp < 32) {
+            document.getElementById("dress").innerHTML = "Wow, it's pretty cold out! You should try wearing layers under your formal wear!"
+        } else if (temp < 55) {
+            document.getElementById("dress").innerHTML = "Kinda chilly, but not too bad. Just throw thick jacket over whatever you're wearing!"
+        }
+    }
+
 }
 
